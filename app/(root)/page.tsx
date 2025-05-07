d05df3e7c8d1b4213cf2072ce1a8f5a2c71edd0c
NodePulse <bharbeysachin@gmail.com>
2025-05-07 22:49:57 +0530

    Designing home page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,3 +1,52 @@
+import InterviewCard from "@/components/InterviewCard";
+import { Button } from "@/components/ui/button";
+import { dummyInterviews } from "@/constants";
+import Image from "next/image";
+import Link from "next/link";
+
 export default function HomePage() {
-  return <div className="">Hello World</div>;
+  return (
+    <>
+      <section className="card-cta">
+        <div className="flex flex-col gap-6 max-w-lg">
+          <h2>
+            Ace Your Interviews with Smart AI Practice and Real-Time Feedback
+          </h2>
+          <p className="text-lg">
+            Refine your answers with real questions and AI-powered feedback
+          </p>
+          <Button asChild className="btn-primary max-sm:w-full">
+            <Link href={"/interview"}>Start an interview</Link>
+          </Button>
+        </div>
+        <Image
+          src={"/robot.png"}
+          alt="robo-dude"
+          width={400}
+          height={400}
+          className="max-sm:hidden"
+        />
+      </section>
+
+      <section className="flex flex-col gap-6 mt-8">
+        <h2>Your interviews</h2>
+        <div className="interviews-section">
+          {/* <p>You haven&apos;t taken any interviews yet</p> */}
+          {dummyInterviews.map((interview) => (
+            <InterviewCard {...interview} key={interview.id} />
+          ))}
+        </div>
+      </section>
+
+      <section className="flex flex-col gap-6 mt-8">
+        <h2>Take an Interview</h2>
+        <div className="interviews-section">
+          {/* <p>There are no interviews available</p> */}
+          {dummyInterviews.map((interview) => (
+            <InterviewCard {...interview} key={interview.id} />
+          ))}
+        </div>
+      </section>
+    </>
+  );
 }
