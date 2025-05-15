e20f925896c54d6100166c88a7b14fe3a60dad72
NodePulse <bharbeysachin@gmail.com>
2025-05-15 19:35:44 +0530

    Adding interview ui

---
+++ app/(root)/interview/page.tsx
@@ -0,0 +1,13 @@
+import Agent from "@/components/Agent";
+import React from "react";
+
+const Page = () => {
+  return (
+    <>
+      <h3>Inteview Generation</h3>
+      <Agent userName="John Doe" />
+    </>
+  );
+};
+
+export default Page;
